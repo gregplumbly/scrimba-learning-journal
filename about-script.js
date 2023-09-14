3424d4389db9da5fa06e778514d9306e480481f1
Greg Plumbly <greg.plumbly@gmail.com>
2023-09-14 16:39:17 +0100

    about js

---
+++ about-script.js
@@ -0,0 +1,19 @@
+import articles from './data.js'
+
+let recentPosts = document.getElementById('recent-posts-grid-container')
+
+// select 3 articles from the array
+let recentArticles = articles.slice(0, 3)
+
+recentArticles.forEach((article) => {
+  recentPosts.innerHTML += `<a href="/article.html?id=${article.id}">
+      <div class="grid-item">
+            <img class="article-image" src="./images/${article.image}" />
+            <p class="article-date">${article.date}</p>
+            <h2>${article.title}</h2>
+            <p class="intro">
+              ${article.intro}
+            </p>
+          </div>
+  </a>`
+})
